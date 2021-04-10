fb45175b95b17e3f353f5ee5caed79fc40f3d315
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2021-04-10 09:54:39 +0500

    Init project

---
+++ main.py
@@ -0,0 +1,116 @@
+import requests
+import telegram
+import logging
+import os
+import time
+from inspect import cleandoc
+from dotenv import load_dotenv
+
+
+logger = logging.getLogger(__file__)
+
+
+DEVMAN_URL = 'https://dvmn.org'
+
+
+def send_message_to_telegram(attempt_info, bot_token, chat_id):
+    """Send a message about a verified attempt to pass the lesson.
+
+    Send a message to the user in telegram about
+    a successful or unsuccessful attempt to pass the lesson.
+    """
+    lesson_title = attempt_info.get('lesson_title')
+    is_negative_result = attempt_info.get('is_negative')
+    lesson_url = 'https://dvmn.org' + attempt_info.get('lesson_url')
+
+    if is_negative_result:
+        decision_comment = 'К сожалению, в работе нашлись ошибки.'
+    else:
+        decision_comment = '''Преподавателю всё понравилось,
+                           можно приступать к следущему уроку!'''
+
+    msg = cleandoc(
+        f'''У вас проверили работу "{lesson_title}"
+        {decision_comment}
+        Посмотреть работу можно по ссылке:
+        {lesson_url}'''
+    )
+
+    bot = telegram.Bot(token=bot_token)
+    bot.send_message(chat_id=chat_id, text=msg)
+
+
+def wait_for_verification_info(url, api_token, timeout=9, pause=30):
+    """Expect devman api to verify lessons.
+
+    Upon successful receipt of a request from the api,
+    send the verification information in telegram to the user.
+    """
+
+    headers = {
+        'Authorization': f'Token {api_token}'
+    }
+    payload = {}
+
+    bot_token = os.getenv('TELEGRAM_TOKEN')
+    chat_id = os.getenv('TG_CHAT_ID')
+
+    # Pause to try to send a repeat request
+    default_pause = 0
+
+    while True:
+        try:
+            response = requests.get(
+                url,
+                headers=headers,
+                timeout=timeout,
+                params=payload
+            )
+            response_data = response.json()
+            logger.info(f'New data:\n {response_data}')
+
+            if response_data.get('status') == 'found':
+                payload['timestamp'] = response_data.get(
+                    'last_attempt_timestamp'
+                )
+                new_attempts = response_data.get('new_attempts')
+                for attempt in new_attempts:
+                    send_message_to_telegram(attempt, bot_token, chat_id)
+            else:
+                payload['timestamp'] = response_data.get(
+                    'timestamp_to_request'
+                )
+        except requests.exceptions.ReadTimeout:
+            logger.warning(
+                'Request timeout. Try to send request again.')
+        except requests.exceptions.ConnectionError:
+            logger_msg = cleandoc(
+                f'''Problems accessing the site.
+                Try to send request again after
+                pause in {default_pause} sec.'''
+            )
+
+            logger.warning(logger_msg)
+
+            # The first attempt to send a repeated request is instant,
+            # then the pause value is set from the function parameters.
+            time.sleep(default_pause)
+            default_pause = pause
+
+
+def main():
+    load_dotenv()
+
+    logging.basicConfig(
+        format='%(levelname)s:%(filename)s:%(message)s',
+        level=logging.DEBUG,
+    )
+
+    url = f'{DEVMAN_URL}/api/long_polling'
+    api_token = os.getenv('DEVMAN_USER_TOKEN')
+
+    wait_for_verification_info(url, api_token)
+
+
+if __name__ == '__main__':
+    main()
